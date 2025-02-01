dabf7310c65a6f3a9b5f87e8880041bc63152782
Hannah Wong <hannah@Hannahs-MacBook-Pro.local>
2025-02-01 18:21:27 -0500

    Adding next to config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,23 @@
 // const isProd = process.env.NODE_ENV === 'production';
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
 
-    return config;
-  },
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: '/Portfolio',
-  basePath: '/Portfolio',
-  output: 'export'
+		return config;
+	},
+	reactStrictMode: true,
+	images: {
+		unoptimized: true, // Disable default image optimization
+	},
+	assetPrefix: '/Portfolio',
+	basePath: '/Portfolio',
+	output: 'export',
 };
 
 export default nextConfig;
